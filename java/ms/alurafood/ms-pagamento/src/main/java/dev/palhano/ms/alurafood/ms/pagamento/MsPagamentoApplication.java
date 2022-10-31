c499afce417078373f42771a3d0b0e727e6a48bb
Vitor Palhano <vitorpalhano2014@gmail.com>
2022-10-31 16:23:20 -0300

    MS:Configured Eureka Server

--- java/ms/alurafood/ms-pagamento/src/main/java/dev/palhano/ms/alurafood/ms/pagamento/MsPagamentoApplication.java
+++ java/ms/alurafood/ms-pagamento/src/main/java/dev/palhano/ms/alurafood/ms/pagamento/MsPagamentoApplication.java
@@ -2,8 +2,10 @@ package dev.palhano.ms.alurafood.ms.pagamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsPagamentoApplication {
 
 	public static void main(String[] args) {
